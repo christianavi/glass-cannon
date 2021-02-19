e9598ac6a341035869442633eabc55a0896b1de2
Christian Avi Bulan <43099141+christianavi@users.noreply.github.com>
2021-02-20 05:18:58 +0800

    Timestamps, party members, and buttons!

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# glass-cannon
-Glass cannon is a tool to help you make Discord rich presence.
+# Glass Cannon
+![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/christianavi/glass-cannon?color=green&logo=github) ![GitHub](https://img.shields.io/github/license/christianavi/glass-cannon) ![Discord](https://img.shields.io/discord/660452758775922701?color=%237289DA&label=Discord&logo=discord&logoColor=white)
+
+Get a custom rich presence on Discord. Change details, state, images, timestamp, and now with <kbd>buttons</kbd>!
+<details>
+    <summary markdown="span">Click to view image previews</summary>
+
+![small](./assets/img/glass-cannon.png)
+![small](./assets/img/rich-presence.png)
+</details>
+
+## Before using
+It is important that you have created your own application on [Discord Developer Portal](https://discord.com/developers/applications).
+> [Learn how to create a Discord application.](https://github.com/christianavi/glass-cannon/wiki/Creating-a-Discord-Application)
+
+## Running Glass Cannon
+> Make sure you have downloaded the latest [release](https://github.com/christianavi/glass-cannon/releases).
+
+The first time you run Glass Cannon, you will be asked to enter the following details about your rich presence:
+* Client ID **(REQUIRED)**
+* Details 
+* State
+* Assets
+    * Large Image Key
+    * Large Image Text
+    * Small Image Key
+    * Small Image Text
+* Timestamp
+    * Start Timestamp
+    * End Timestamp
+* Party Information
+    * Party ID
+    * Party Size
+    * Max Party Members allowed
+    * Join Secret - **Ask to join button**
+* Buttons - **Your own custom button**
+
+There will be an indicator if the values asked are required or not. If not required, you can simply press <kbd>Enter</kbd> to skip.
+> You cannot use Join Secret and Buttons at the same time. [Learn more](https://github.com/christianavi/glass-cannon/wiki/Buttons-vs-Join-Secrets).
+
+After entering the required details, you will be asked if you want to start the presence already. If not, Glass Cannon will exit.
+
+The next time you run Glass Cannon, you will not be asked to enter all those details again. Glass Cannon actually saves your previous rich presence details. Instead, you will be asked if you want to use the previous settings or not.
+## FAQs
+### Why is the presence did not updateing or showing up? Why is it "not working?"
+Two things you can do is to
+1. Make sure that you are online and not invisible, and the
+2. Turn on the **Display currently running game as a status message** option found on Settings > Game Activity.
+
+### Why are images not appearing?
+Discord usually takes a few minutes to cache your application assets. Wait a few minutes and run the presence again.
+
+### Why are my buttons not working when clicked?
+Due to Discord limitation, you cannot click your own buttons. Rest assured, it works on everybody else.
+
+## Still need help?
+Join us on [Discord](https://discord.gg/NkPy7se).
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const RPC = require('discord-rpc');
-const chalk = require('chalk');
 const fs = require('fs')
+const chalk = require('chalk');
 const prompt = require('prompt-sync')();
+const { Client } = require('discord-rpc');
 const package = require(`./package.json`);
+const { start } = require('repl');
 
 let showtip = false
-let clsPvs = true
 
 function header() {
-    if(clsPvs == true) { console.clear() }
-    console.log(chalk.bold(`${package.name}\n`))
+    console.clear()
+    console.log(chalk.bold(`${package.name} ` + chalk.dim(` ${package.version} `)))
     if (showtip == true){
             console.log(chalk.red('*') + ' - Required. Cannot be blank.\n' + chalk.green('*') + ' - Not required. Can be left blank. Press Enter to skip.\n')
         }
@@ -17,55 +17,74 @@ function header() {
 
 function init(options) {
 
-    const client = new RPC.Client({ transport: 'ipc' })
+    const client = new Client({ transport: 'ipc' })
     const activity = {}
-    const assets = {}
-
-    if (options.largeimage !== '') {
-        assets.large_image = options.largeimage
-        if (options.largeImagetext == ''){
-            assets.large_text = `${package.name} by ${package.author}`
-        }
-        else {
-            assets.large_text = options.largeImagetext
-        }     
-    }
-    if (options.smallimage !== '') {
-        assets.small_image = options.smallimage
-        if (options.smallImagetext == ''){
-            assets.small_text = `${package.repository.url}`
-        }
-        else {
-            assets.small_text = options.smallImagetext
-        }     
+    
+    activity.clientId = options.clientId
+    if (options.details !== '') { activity.details = options.details }
+    if (options.state !== '') { activity.state = options.state }
+    if (options.startTimestamp) {
+        activity.startTimestamp = parseInt(options.startTimestamp)
+        if (options.endTimestamp) { activity.endTimestamp = parseInt(options.endTimestamp) }
     }
 
-    if (assets !== {}) {
-        activity.assets = assets
+    if (options.largeImageKey !== '') {
+        activity.largeImageKey = options.largeImageKey
+        if (options.largeImageText !== ''){
+            activity.largeImageText = options.largeImageText    
+        }
+        else {
+            activity.largeImageText = `${package.name} by ${package.author}`
+        }     
     }
-    if (options.description !== '') {
-        activity.details = options.description
+    if (options.smallImageKey !== '') {
+        activity.smallImageKey = options.smallImageKey
+        if (options.smallImageText !== ''){
+            activity.smallImageText = options.smallImageText
+        }
+        else {
+            activity.smallImageText = `${package.repository.url}`
+        }     
     }
-    if (options.state !== '') {
-        activity.state = options.state
+    if (options.partyId) { activity.partyId = options.partyId } 
+    if (options.joinSecret && options.buttons[0]) {
+        let response = 0
+        header()
+        console.log(chalk.yellow("It appears that the profile has both joinSecret and buttons. Secrets cannot currently be sent with buttons, so you will have to select which one to use.") + "\n1 - None\n2 - joinSecret " + chalk.dim('(Ask to join button)') + "\n3 - Buttons")
+        response = Math.floor(prompt("Enter the number of your choice: "))
+        if (response == '' || isNaN(response) || response < 1 || response > 3) { console.log("Invalid number. Defaulting to 1 - none.") }
+        else if (response == 1) { }
+        else if (response == 2) { activity.joinSecret = options.joinSecret }
+        else if (response == 3) {
+            activity.buttons = options.buttons
+        }
+        else { console.log(chalk.red('This is something I haven\'t forseen.')) }
     }
-    if (options.buttons.length !== 0) {
-        activity.buttons = options.buttons
+    else if (options.joinSecret) { activity.joinSecret = options.joinSecret }
+    else if (options.buttons[0]) { activity.buttons = options.buttons }
+    else { console.log(chalk.red('This is something I haven\'t forseen.')) }
+
+
+    if (options.partySize && options.partyMax) {
+        activity.partySize = parseInt(options.partySize)
+        activity.partyMax = parseInt(options.partyMax)
     }
+
+    activity.instance = false
 
     client.on('ready', () => {
-        client.request('SET_ACTIVITY', {
-            pid: process.pid,
-            activity: activity
-        })
-
+        if (!activity.startTimestamp) {
+            const startTimestamp = new Date();
+            activity.startTimestamp = startTimestamp
+        }
+        client.setActivity(activity)
         showtip = false
-        clsPvs = false
         header()
+        summary(activity)
         console.log(chalk.green('Your rich presence has started.'))
     })
 
-    client.login({ clientId: options.clientid })
+    client.login({ clientId: activity.clientId })
 };
 
 function create() {
@@ -76,12 +95,18 @@ function create() {
     }
 
     let clientId = 0
-    let largeImage = ''
-    let largeImagetext = ''
-    let smallImage = ''
-    let smallImagetext = ''
-    let description = ''
+    let details = ''
     let state = ''
+    let startTimestamp = ''
+    let endTimestamp = ''
+    let largeImageKey = ''
+    let largeImageText = ''
+    let smallImageKey = ''
+    let smallImageText = ''
+    let partyId = ''
+    let partySize = 0
+    let partyMax = 0
+    let joinSecret = 0
     let buttoncount = 0
     let buttononelabel = ''
     let buttontwolabel = ''
@@ -92,11 +117,10 @@ function create() {
     
     showtip = true
     header()
-
+    // clientId
     while(!isnum){
         clientId = prompt(chalk.red('*') + ' Enter Application Client ID: ' )
-        if(clientId === '' || isNaN(clientId)){
-            
+        if(clientId === '' || isNaN(clientId)){    
             header()
             console.log(chalk.red("Your Client ID has to be a number."))
         }
@@ -105,19 +129,59 @@ function create() {
         }
     }
 
+    isnum = false
+    
     header()
-    largeImage = prompt(chalk.green('*') + ' Enter large image asset name: ')
-    if(largeImage){
-        largeImagetext = prompt(chalk.green('*') + ' Enter large image text: ')
-        smallImage = prompt(chalk.green('*') + ' Enter small image asset name: ')
-        if(smallImage){
-            smallImagetext = prompt(chalk.green('*') + ' Enter small image text: ')
+    details = prompt(chalk.green('* ') + 'Enter the Details of your presence: ')
+    state = prompt(chalk.green('* ') + 'Enter the State of your presence: ')
+ 
+    header()
+    largeImageKey = prompt(chalk.green('* ') + 'Enter large image asset name: ')
+    if(largeImageKey){
+        largeImageText = prompt(chalk.green('* ' ) + 'Enter large image text: ')
+        smallImageKey = prompt(chalk.green('* ') + 'Enter small image asset name: ')
+        if(smallImageKey){
+            smallImageText = prompt(chalk.green('* ') + 'Enter small image text: ')
         }
     }
 
     header()
-    description = prompt(chalk.green('*') + ' Enter the Details of your presence: ')
-    state = prompt(chalk.green('*') + ' Enter the State of your presence: ')
+    console.log("Timestamps are in Unix time. Go to " + chalk.underline('https://www.epochconverter.com/') + " to convert human readable time to Unix time.")
+    startTimestamp = prompt(chalk.green('* ') + "Enter Start Timestamp: ")
+    if (isNaN(startTimestamp)) {
+        startTimestamp = ''
+    }
+    if (startTimestamp !== ''){
+        endTimestamp = prompt(chalk.red('* ') + "Enter End Timestamp: ")
+        if (isNaN(endTimestamp)) {
+            startTimestamp = ''
+            endTimestamp = ''
+        }
+    }
+
+    header()
+    partyId = prompt(chalk.green('* ') + 'Enter the your Party ID: ')
+    if (partyId) {
+        joinSecret = prompt(chalk.green('* ') + 'Enter your Join Secret: ')
+    }
+
+    header()
+    partySize = prompt(chalk.green('* ') + 'Enter the Party Size: ')
+    if (!isNaN(partySize)) {
+        if (partySize) {
+            while (!isnum) {
+                partyMax = prompt(chalk.red('* ') + 'Enter the Party Max: ')
+                if (partySize > partyMax) {
+                    console.log(chalk.red("Party Max cannot exceed Party Size (" + partySize  + ")."))
+                }
+                if (isNaN(partyMax)) {
+                    console.log(chalk.red(partyMax  + " is not a valid number."))
+                }
+                else { isnum = true }
+            }
+        }
+    }
+    else { partySize = 0 }
 
     header()
     buttoncount = Math.floor(prompt(chalk.green('* ') + "Enter the number of buttons you would want. (0-2) "))
@@ -177,13 +241,19 @@ function create() {
     if (buttontwo !== null) { buttons.push(buttontwo) }
 
     const content = {
-        clientid: clientId,
-        description: description,
+        clientId: clientId,
+        details: details,
         state: state,
-        largeimage: largeImage,
-        largeImagetext: largeImagetext,
-        smallimage: smallImage,
-        smallImagetext: smallImagetext,
+        startTimestamp: startTimestamp,
+        endTimestamp: endTimestamp,
+        largeImageKey: largeImageKey,
+        largeImageText: largeImageText,
+        smallImageKey: smallImageKey,
+        smallImageText: smallImageText,
+        partyId: partyId,
+        partySize: partySize,
+        partyMax: partyMax,
+        joinSecret: joinSecret,
         buttons: buttons
     }
     
@@ -191,7 +261,7 @@ function create() {
 
     const data = JSON.stringify(content, null, 2)
 
-    fs.writeFile(`./rpc.json`, data, (err) => {
+    fs.writeFile('./rpc.json', data, (err) => {
         if (err) { throw err }
     })
 
@@ -209,20 +279,26 @@ function create() {
 function summary(options) {
     // console.log(options)
     console.log(chalk.green('Rich Presence Summary'))
-    console.log(`Client ID:         ${options.clientid}`)
-    if (options.largeimage !== ''){ console.log(`Large Image Asset: ${options.largeimage}`) }
-    if (options.largeimagetext) { console.log(`Large Image Text:  ${options.largeimagetext}`) } else { console.log(`Large Image Text:  ${package.name} by ${package.author} ` + chalk.bgGreen(' Default ')) }
-    if (options.smallimage !== '') { console.log(`Small Image Asset: ${options.smallimage}`) }
-    if (options.smallimagetext) { console.log(`Small Image Text:  ${options.smallimagetext}`) }
-    if (options.description !== '') { console.log(`Details:           ${options.description}`) }
-    if (options.state !== '') { console.log(`State:             ${options.state}`) }
-    if (options.buttons[0]) {
+    console.log(`Client ID:         ${options.clientId}`)
+    if (options.details !== '') { console.log(`Details:           ${options.details}`) }
+    if (options.state) { console.log(`State:             ${options.state}`) } else { }
+    if (options.largeImageKey !== ''){ console.log(`Large Image Asset: ${options.largeImageKey}`) }
+    if (options.largeImageText) { console.log(`Large Image Text:  ${options.largeImageText}`) } else { console.log(`Large Image Text:  ${package.name} by ${package.author} ` + chalk.dim('Default')) }
+    if (options.smallImageKey) { console.log(`Small Image Asset: ${options.smallImageKey}`) }
+    if (options.smallImagetext) { console.log(`Small Image Text:  ${options.smallImagetext}`) }
+    if (options.partyId) { console.log(`Party ID:          ${options.partyId}`) }
+    if (options.joinSecret){ console.log(`Join secret:       ${options.joinSecret}`) }
+    if (options.partySize) { console.log(`Party size:        ${options.partySize}`) }
+    if (options.partyMax) { console.log(`Max party members: ${options.partyMax}`) }
+    if (options.buttons) {
+        if (options.buttons[0]) {
         if (options.buttons[0].label) { console.log(`Button 1 Label:    ${options.buttons[0].label}`) }
         console.log(`Button 1 URL:      ${options.buttons[0].url}`)
         }
-    if (options.buttons[1]) {
-        if (options.buttons[1].label) { console.log(`Button 2 Label:    ${options.buttons[1].label}`) }
-        console.log(`Button 2 URL:      ${options.buttons[1].url}`)
+        if (options.buttons[1]) {
+            if (options.buttons[1].label) { console.log(`Button 2 Label:    ${options.buttons[1].label}`) }
+            console.log(`Button 2 URL:      ${options.buttons[1].url}`)
+        }
     }
     console.log() // Empty line
 }
